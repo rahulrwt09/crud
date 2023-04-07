0ecfa25e30f4b50729201276508814e20d5d8d26
rahul rawat <golubook09@gmail.com>
2023-04-07 06:27:52 +0100

    add router in app.js and component also in this router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from './Main/Main';
+import Product from './admin/Product';
+import Productedit from './admin/Productedit';
+import Productcreate from './admin/Productcreate';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome from app.js</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Main />} />
+          <Route path='/admin/products' exact element={<Product />} />
+          <Route path='/admin/products/:id/edit' exact element={<Productedit />} />
+          <Route path='/admin/products/create' exact element={<Productcreate />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
